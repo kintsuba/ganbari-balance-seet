1889fb88ef4c00160f729bda9420b9d2a330b2bf
Kintsuba <bnxth107@gmail.com>
2024-10-23 05:17:47 +0900

    feat: support nuxt4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: "2024-10-23",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ["@nuxt/ui"],
+});
